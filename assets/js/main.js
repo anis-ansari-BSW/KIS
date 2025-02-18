8358f57b156c2906249696c302cd25bd49ecf78b
anis-ansari-BSW <anis.ansari@brand-scapes.com>
2025-02-18 16:25:23 +0530

    all page added

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,4 +185,41 @@
 
   window.addEventListener("load", initSwiper);
 
+   // Initiate the wowjs
+   new WOW().init();
+
+   // Testimonial-carousel
+   $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: false,
+    loop: true,
+    margin: 25,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:2
+        },
+        1200:{
+            items:2
+        }
+    }
+});
+
+
 })();
